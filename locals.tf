b0aab1a21fbfc610e8f6519bfb6693ba6682ddef
data909 <tarkanic909@gmail.com>
2026-05-24 17:35:49 +0200

    tf(locals): move some entries to vars

--- locals.tf
+++ locals.tf
@@ -1,9 +1,22 @@
 locals {
-  prefix = "k8s-lab"
+  vms = var.single_node ? {
+    master = { subnet = "master", is_master = true }
+    } : {
+    master  = { subnet = "master", is_master = true }
+    worker1 = { subnet = "workers", is_master = false }
+    worker2 = { subnet = "workers", is_master = false }
+  }
 
-  vms = {
-    master  = { subnet = "master" }
-    worker1 = { subnet = "workers" }
-    worker2 = { subnet = "workers" }
+  os_image = {
+    publisher = "Debian"
+    offer     = "debian-12"
+    sku       = "12-gen2"
+  }
+
+  vnet_cidr = "10.0.0.0/16"
+
+  subnet_cidrs = {
+    master  = "10.0.1.0/24"
+    workers = "10.0.2.0/24"
   }
 }
